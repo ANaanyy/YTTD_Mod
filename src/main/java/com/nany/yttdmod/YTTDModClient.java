9cc1f154590cdf9a64ee683537f03f24c3f8c485
ANaanyy <lojonessantos@gmail.com>
2026-01-28 18:19:10 -0300

    basico que qualquer mod tem que ter pra começar a programar

---
+++ src/main/java/com/nany/yttdmod/YTTDModClient.java
@@ -0,0 +1,10 @@
+package com.nany.yttdmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class YTTDModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/nany/yttdmod/YTTDModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.nany.yttdmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class YTTDModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
